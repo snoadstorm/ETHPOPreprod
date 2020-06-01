042a214cc4b8d7bc26b4cb4c1867bc30dd3fcb2d
Dr Nathan Green <n8thangreen@yahoo.co.uk>
2020-06-01 21:57:32 +0100

    start to generalise

--- scripts/check_inflow_outflow_each_year.R
+++ scripts/check_inflow_outflow_each_year.R
@@ -1,53 +1,66 @@
 
 ## prep data
 
+# test for subset of population
+ETHNICGRP <- "Bangladeshi"
+UKBORN <- "Non-UK born"
+
 dat <-
   sim_pop %>%
-  filter(ETH.group == "Bangladeshi",
+  filter(ETH.group == ETHNICGRP,
          sex == "M",
-         CoB == "Non-UK born")
+         CoB == UKBORN)
 
 dat_d <-
   dat_deaths %>%
   mutate(agegrp = cut(age, seq(0,100,by = 5), right = FALSE)) %>%
   group_by(agegrp, sex, ETH.group, year, CoB) %>%
   summarise(pop = sum(deaths)) %>%
-  filter(ETH.group == "Bangladeshi",
+  filter(ETH.group == ETHNICGRP,
          sex == "M",
-         CoB == "Non-UK born")
+         CoB == UKBORN)
 dat_in <-
   dat_inflow %>%
   mutate(agegrp = cut(age, seq(0,100,by = 5), right = FALSE)) %>%
   group_by(agegrp, sex, ETH.group, year, CoB) %>%
   summarise(pop = sum(inmigrants)) %>%
-  filter(ETH.group == "Bangladeshi",
+  filter(ETH.group == ETHNICGRP,
          sex == "M",
-         CoB == "Non-UK born")
+         CoB == UKBORN)
 
 dat_out <-
   dat_outflow %>%
   mutate(agegrp = cut(age, seq(0,100,by = 5), right = FALSE)) %>%
   group_by(agegrp, sex, ETH.group, year, CoB) %>%
   summarise(pop = sum(outmigrants)) %>%
-  filter(ETH.group == "Bangladeshi",
+  filter(ETH.group == ETHNICGRP,
          sex == "M",
-         CoB == "Non-UK born")
+         CoB == UKBORN)
+
+# dat_b <-
+#   dat_births %>%
+#   filter(ETH.group == "Bangladeshi")
 
 
 ## main loop
 
-lage <- 10#5
-uage <- 14#9
-agegroup <- "[10,15)"#"[5,10)"
+lage <- 5
+uage <- 9
+agegroup <- "[5,10)"
 out <- NULL
 
 for (year in 2012:2050){
 
-  age_in  <- dat$pop[dat$year == year - 1   & dat$age == lage - 1]
+  age_in  <- if (lage == 0) {
+    dat_b$pop[dat_b$year == year]                        # births
+  } else {
+    dat$pop[dat$year == year - 1 & dat$age == lage - 1]  # previous year, one year younger
+  }
+
   age_out <- dat$pop[dat$year == year       & dat$age == uage]
   death   <- dat_d$pop[dat_d$year == year   & dat_d$agegrp == agegroup]
-  inflow  <- dat_in$pop[dat_d$year == year  & dat_d$agegrp == agegroup]
-  outflow <- dat_out$pop[dat_d$year == year & dat_d$agegrp == agegroup]
+  inflow  <- dat_in$pop[dat_in$year == year  & dat_in$agegrp == agegroup]
+  outflow <- dat_out$pop[dat_out$year == year & dat_out$agegrp == agegroup]
 
   out <- rbind(out, c(year,
                       ifelse(length(age_in) > 0, age_in, NA),
@@ -72,3 +85,6 @@ counts <- rbind(
 barplot(counts, main = "",
         xlab = "", col=c("blue","red"),
         legend = rownames(counts))
+
+
+
